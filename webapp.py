02f63da9af352f033a437575dc169e1cb9eebce6
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-15 09:57:02 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,7 +13,12 @@ def render_main():
 def render_p1():
   with open('police_shootings (1).json') as shooting_data:
     counties = json.load(shooting_data)
-  return render_template('page1.html')
+  return render_template('page1.html', shootingOverTimeData = get_shootingsOverTimeData(shooting_data))
+  
+def get_shootingsOverTimeDate(dataSet):
+  toReturn = [{x:1, y:1},{x:2, y:2}]
+  return toReturn
+    
   
 if __name__=="__main__":
     app.run(debug=False)
