0736e2b644ec3ce29adbdecbc8ee358e2fe524d4
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-18 09:32:06 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,7 +14,14 @@ def render_p1():
   with open('police_shootings (1).json') as shooting_data:
     counties = json.load(shooting_data)
   return render_template('page1.html', numWhite = 20, numBlack = 20, numAsian = 20, numNA = 20, numHispanic = 20, numOther = 20, numUnknown =20)
-    
+
+def count_individuals(category, specificCategory, target, data):
+  toReturn = 0
+  for Incident in data:
+    if data[category][specificCategory] == target:
+      toReturn +=1
+  return toReturn
+
   
 if __name__=="__main__":
     app.run(debug=False)
