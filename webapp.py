e8a0a1b82d30886a5f82930e78193bd3a5997071
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-21 09:06:48 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -31,7 +31,7 @@ def get_Arms(data):
   for incident in data:
     if incident["Factors"]["Armed"] not in list:
       list[incident["Factors"]["Armed"]]=1
-    elif:
+    else:
       list[incident["Factors"]["Armed"]]+=1
    print(list)
   
